9622a1250b9f6599d878bcb2e5a54bc0815a91af
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-10-29 20:10:58 -0300

    Criando o serviço

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getWeather(){
+    return this.httpClient.get('https://api.hgbrasil.com/weather?key=6a54a76a&city_name=Concei%C3%A7%C3%A3o_dos_Ouros');
+  }
+
 }
